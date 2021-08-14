6b258c326110f3d495d34d8655e489264597c7f6
Rafael <rafa.s.francisco@gmail.com>
2021-08-14 11:52:15 -0300

    Trabalhando com spring data.

--- src/main/java/br/com/multbroker/avaliacaoMultbroker/controller/PessoaController.java
+++ src/main/java/br/com/multbroker/avaliacaoMultbroker/controller/PessoaController.java
@@ -2,9 +2,6 @@ package br.com.multbroker.avaliacaoMultbroker.controller;
 
 import java.util.List;
 
-import javax.persistence.NoResultException;
-import javax.transaction.Transactional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,67 +11,65 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.multbroker.avaliacaoMultbroker.converter.ConversorPessoa;
-import br.com.multbroker.avaliacaoMultbroker.dao.PessoaDao;
 import br.com.multbroker.avaliacaoMultbroker.model.Pessoa;
+import br.com.multbroker.avaliacaoMultbroker.model.PessoaRepository;
 
 @RestController
+@RequestMapping("/pessoas")
 public class PessoaController {
 	
 	@Autowired
-	private PessoaDao pessoaDao;
+	private PessoaRepository repository; 
 	
-	@GetMapping("/pessoa")
+	@GetMapping
 	public ResponseEntity<List<Pessoa>> lista() {
 		
-		if (pessoaDao.findAll().isEmpty()) {
+		if (repository.findAll().isEmpty()) {
 			return new ResponseEntity<List<Pessoa>>(HttpStatus.NO_CONTENT);
 		}
 		
-		return new ResponseEntity<List<Pessoa>>(pessoaDao.findAll(), HttpStatus.OK);
+		return new ResponseEntity<List<Pessoa>>(repository.findAll(), HttpStatus.OK);
 	}
 	
-	@GetMapping("/pessoa/{id}")
+	@GetMapping("/{id}")
 	public ResponseEntity<Pessoa> buscaPorId(@PathVariable("id") Long id) {
 		
-		if (pessoaDao.findById(id) != null) {
-			return new ResponseEntity<Pessoa>(pessoaDao.findById(id), HttpStatus.OK);
-		} else {
-			return new ResponseEntity<Pessoa>(HttpStatus.NO_CONTENT);
-		}
+		if (repository.findById(id).isPresent()) {
+			return new ResponseEntity<Pessoa>(repository.findById(id).get(), HttpStatus.OK);
+		} 
+			
+		return new ResponseEntity<Pessoa>(HttpStatus.NO_CONTENT);
 	}
 	
-	@GetMapping("/pessoa")
+	@GetMapping("/busca-por/")
 	public ResponseEntity<Pessoa> buscaPorNome(@RequestParam("nome") String nome) {
-		
-		try {
-			Pessoa pessoa = pessoaDao.findByName(nome);
 			
-			return new ResponseEntity<Pessoa>(pessoa, HttpStatus.OK);
-			
-		} catch(NoResultException e) {
-			return new ResponseEntity<Pessoa>(HttpStatus.NO_CONTENT);
+		if (repository.findByNome(nome.toLowerCase()) != null) {			
+			return new ResponseEntity<Pessoa>(repository.findByNome(nome.toLowerCase()), HttpStatus.OK);
 		}
+		
+		return new ResponseEntity<Pessoa>(HttpStatus.NO_CONTENT);
 
 	}
 	
-	@Transactional
-	@PostMapping("/pessoa")
+	@PostMapping
 	public ResponseEntity<Pessoa> cria(@RequestBody Pessoa novaPessoa) {
 		Pessoa pessoa = new ConversorPessoa().moveEconverteMinusculo(null, novaPessoa);
-		pessoaDao.create(pessoa);
+		repository.save(pessoa);
+		
 		return new ResponseEntity<Pessoa>(pessoa, HttpStatus.CREATED);
 	}
 	
-	@Transactional
-	@DeleteMapping("/pessoa/{id}")
+	@DeleteMapping("/{id}")
 	public ResponseEntity<Pessoa> deleta(@PathVariable("id") Long id) {
 		
-		if (pessoaDao.findById(id) != null) {
-			pessoaDao.delete(pessoaDao.findById(id));
+		if (repository.findById(id).isPresent()) {
+			repository.delete(repository.findById(id).get());
 			
 			return new ResponseEntity<Pessoa>(HttpStatus.OK);
 		}
@@ -82,14 +77,14 @@ public class PessoaController {
 		return new ResponseEntity<Pessoa>(HttpStatus.NO_CONTENT);
 	}
 	
-	@Transactional
-	@PutMapping("/pessoa/{id}")
+	
+	@PutMapping("/{id}")
 	public ResponseEntity<Pessoa> atualiza(@PathVariable("id") Long id, @RequestBody Pessoa alteraPessoa) {
 		
-		if (pessoaDao.findById(id) != null) {
-			Pessoa pessoa = new ConversorPessoa().moveEconverteMinusculo(pessoaDao.findById(id), alteraPessoa);
+		if (repository.findById(id).isPresent()) {
+			Pessoa pessoa = new ConversorPessoa().moveEconverteMinusculo(repository.findById(id).get(), alteraPessoa);
 			
-			pessoaDao.update(pessoa);
+			repository.save(pessoa);
 			
 			return new ResponseEntity<Pessoa>(pessoa, HttpStatus.OK);
 		}
--- src/main/java/br/com/multbroker/avaliacaoMultbroker/model/PessoaRepository.java
+++ src/main/java/br/com/multbroker/avaliacaoMultbroker/model/PessoaRepository.java
@@ -5,5 +5,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PessoaRepository extends JpaRepository<Pessoa, Long> {
+	
+	public Pessoa findByNome(String nome);
 
 }
